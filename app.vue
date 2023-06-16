ea73b9cd8df05ddb440060d500ca5910904c9d63
Em Dahlström <em@djurensratt.se>
2023-06-16 09:22:55 +0200

    Adds seo meta

--- app.vue
+++ app.vue
@@ -3,3 +3,27 @@
     <NuxtWelcome />
   </div>
 </template>
+<script setup lang="ts">
+import { useRoute, useRuntimeConfig, useLazyAsyncData, useAsyncData, useSeoMeta, useHead, useServerSeoMeta } from '#imports';
+
+useServerSeoMeta({
+  title: 'A title',
+  description: 'A description',
+  canonical: 'https://example.com',
+  lang: 'en',
+  ogImage: 'https://res.cloudinary.com/klandestino-ab/image/upload/c_fill,w_1200,h_630,q_auto,f_auto/djurensratt-se/loggor/share_1200x630',
+  ogUrl: () => 'https://example.com',
+  twitterCard: 'summary_large_image',
+  twitterImage: 'https://res.cloudinary.com/klandestino-ab/image/upload/c_fill,w_1200,h_630,q_auto,f_auto/djurensratt-se/loggor/share_1200x630',
+  abstract: 'A description',
+  ogTitle: 'A title',
+  ogDescription: 'A description',
+  ogType: 'website',
+  twitterTitle: 'A title',
+  twitterDescription: 'A description',
+  twitterUrl: 'https://example.com',
+  twitterSite: '@example',
+  robots: 'index,follow',
+});
+
+</script>
